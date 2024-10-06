1c3297cc4fae66f9953e279a1ce48fa4ae7b199e
dassuraj <dassuraj.np@gmail.com>
2024-10-06 16:31:06 +0545

    initial projecct setup

---
+++ src/main/java/com/security/webtech/digitalnepalfullstackcrud/DigitalNepalFullstackCrudApplication.java
@@ -0,0 +1,13 @@
+package com.security.webtech.digitalnepalfullstackcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalNepalFullstackCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalNepalFullstackCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/webtech/digitalnepalfullstackcrud/DigitalNepalFullstackCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.webtech.digitalnepalfullstackcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalNepalFullstackCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
